e6830634a69dcbef303e3ec804d6259e9638725b
alex.paclin <alex.paclin@gmail.com>
2020-04-10 15:11:02 +0500

    18. Вывод списка постов

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,12 +1,34 @@
-import { Component, OnInit } from "@angular/core";
+import { Subscription } from 'rxjs';
+import { Post } from 'src/app/shared/interfaces';
+
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { PostsService } from '../../shared/posts.service';
 
 @Component({
-	selector: "app-dashboard-page",
-	templateUrl: "./dashboard-page.component.html",
-	styleUrls: ["./dashboard-page.component.scss"],
+	selector: 'app-dashboard-page',
+	templateUrl: './dashboard-page.component.html',
+	styleUrls: ['./dashboard-page.component.scss'],
 })
-export class DashboardPageComponent implements OnInit {
-	constructor() {}
+export class DashboardPageComponent implements OnInit, OnDestroy {
+	posts: Post[] = [];
+	pSub: Subscription;
 
-	ngOnInit(): void {}
+	constructor(private postsService: PostsService) {}
+
+	ngOnInit(): void {
+		this.pSub = this.postsService.getAll().subscribe((posts) => {
+			this.posts = posts;
+		});
+	}
+
+	ngOnDestroy(): void {
+		if (this.pSub) {
+			this.pSub.unsubscribe();
+		}
+	}
+
+	remove(id: string) {
+
+	}
 }
