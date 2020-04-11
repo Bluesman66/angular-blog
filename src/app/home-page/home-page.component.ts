fbf303087616b67073ea71cf99dbaa720a2f9470
alex.paclin <alex.paclin@gmail.com>
2020-04-11 23:04:01 +0500

    25. Вывод постов

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,15 +1,21 @@
+import { Observable } from 'rxjs';
+
 import { Component, OnInit } from '@angular/core';
 
+import { Post } from '../shared/interfaces';
+import { PostsService } from '../shared/posts.service';
+
 @Component({
-  selector: 'app-home-page',
-  templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+	selector: 'app-home-page',
+	templateUrl: './home-page.component.html',
+	styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent implements OnInit {
+	posts$: Observable<Post[]>;
 
-  constructor() { }
+	constructor(private postsService: PostsService) {}
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {
+		this.posts$ = this.postsService.getAll();
+	}
 }
